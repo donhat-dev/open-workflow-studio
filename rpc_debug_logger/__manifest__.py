7095c4266e5ccdd48449c2cd29fe88019788e587
Nguyễn Quỳnh Duyên <duyennqk24416@st.uel.edu.vn>
2026-04-08 23:49:42 +0700

    [ADD] Implement RPC Debug Logger with XML-RPC and JSON-RPC request logging

---
+++ rpc_debug_logger/__manifest__.py
@@ -0,0 +1,10 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    "name": "RPC Debug Logger",
+    "summary": "Log RPC payload and response around base RPC controllers",
+    "version": "1.0",
+    "category": "Hidden",
+    "depends": ["base"],
+    "installable": True,
+    "license": "LGPL-3",
+}
